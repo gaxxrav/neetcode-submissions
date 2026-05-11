0165297fa2c8935ab42c0203dddf831967c46d8b
Gaurav <120584612+gaxxrav@users.noreply.github.com>
2026-05-11 16:39:49 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned = []
+
+
+        #manual cleaning
+        
+        for ch in s:
+            if ch.isalnum():
+                cleaned.append(ch.lower())
+
+        return cleaned == cleaned[::-1]
